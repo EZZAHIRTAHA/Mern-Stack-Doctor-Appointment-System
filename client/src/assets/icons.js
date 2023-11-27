3c09ac35ac9f12136ea97f957286c9cef65d9fbc
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-11-27 22:30:53 +0100

    Feat: Sidebar

--- client/src/assets/icons.js
+++ client/src/assets/icons.js
@@ -1,8 +1,25 @@
 import { FaRegUser } from "react-icons/fa";
-
 import { BiShow, BiHide } from "react-icons/bi";
+import { AiOutlineShoppingCart, AiOutlineMenu } from 'react-icons/ai';
+import { BsCalculatorFill, BsFillPieChartFill } from 'react-icons/bs';
+import { FaHandHoldingMedical, FaBookMedical, FaClinicMedical } from 'react-icons/fa';
+import {FaHospitalUser} from 'react-icons/fa6';
+import {GrClose} from 'react-icons/gr';
+import { RxDashboard } from 'react-icons/rx';
+
+
 
 
 export const User = FaRegUser 
 export const Show = BiShow
 export const Hide = BiHide
+export const ShoppingCartIcon = AiOutlineShoppingCart;
+export const CalculatorIcon = BsCalculatorFill;
+export const PieChartIcon = BsFillPieChartFill;
+export const MedicalIcon = FaHandHoldingMedical;
+export const MedicalFolder = FaBookMedical; 
+export const Pharmacie = FaClinicMedical;
+export const Admission = FaHospitalUser;
+export const MenuBar = AiOutlineMenu;
+export const Close = GrClose;
+export const DashboardIcon = RxDashboard;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,60 @@
-import {} from 'react'
-import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Link } from 'react-router-dom'; 
+import {
+  DashboardIcon, ShoppingCartIcon, CalculatorIcon, PieChartIcon, MedicalFolder, Pharmacie
+} from '../assets/icons';
+import { BiUser } from 'react-icons/bi';
 
-const Home = () => {
-    
-
+const Dashboard = () => {
+  const menuItem = [
+    {
+      path: "/",
+      name: "Dashboard",
+      icon: <DashboardIcon />,
+    },
+    {
+      path: "/users",
+      name: "Users",
+      icon: <BiUser />,
+    },
+    {
+      path: "/comptabilite",
+      name: "Comptabilité",
+      icon: <CalculatorIcon />,
+    },
+    {
+      path: "/statistiques",
+      name: "Statistiques",
+      icon: <PieChartIcon />,
+    },
+    {
+      path: "/dossiers_patient",
+      name: "Dossiers patient",
+      icon: <MedicalFolder />,
+    },
+    {
+      path: "/pharmacie",
+      name: "Pharmacie",
+      icon: <Pharmacie />,
+    },
+  ];
 
   return (
-    <div className='h-screen flex justify-start w-[20%]  bg-indigo-950  text-indigo-500'>
-      <div className="flex justify-start gap-5 text-center  flex-col">
-      </div>
-    </div>
-  )
+    <Sidebar className=' flex h-screen w-full m-10 bg-gray-800 text-white justify-center items-center'>
+      <Menu className='flex p-4 rounded-xl bg-gray-800 w-full  justify-center items-center'>
+        {menuItem.map((item, index) => (
+          <Link to={item.path} className='flex text-xl w-full justify-center items-center' key={index}>
+              <span>
+                {item.icon}
+                </span>
+            <MenuItem className='w-full'>
+              {item.name}
+              </MenuItem>
+          </Link>
+        ))}
+      </Menu>
+    </Sidebar>
+  );
 }
 
-export default Home
+export default Dashboard;
