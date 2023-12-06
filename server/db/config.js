d83c0f2baa14e7ab8313c9e2f4680d7470626be1
EZZAHIR TAHA <taha1.ezzahir@gmail.com>
2023-12-06 15:40:08 +0100

    Chore: switch database connection to MongoDB Atlas

--- server/db/config.js
+++ server/db/config.js
@@ -1,18 +1,17 @@
-
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 const connectDb = async () => {
-
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/mernHis');
+        await mongoose.connect(process.env.MONGODB_URI, {
+            // Other mongoose options if needed
+        });
 
-        console.log('➜ Database:    '.bold +  `mongodb://${mongoose.connection.host}:${mongoose.connection.port}`.green.underline);
+        console.log('➜ Database:    '.bold +  `Connected to MongoDB Cloud`.green.underline);
         console.log('➜ Author:    '.bold +  "Ezzahir Taha".rainbow.bold);
-
     } catch (err) {
-
         console.error('⚠️ Error connecting to the database:'.red.bold, err.message);
-    
     }
 };
 
--- server/server.js
+++ server/server.js
@@ -21,3 +21,4 @@ app.get("/", (req, res) => {
 });
 
 connectDb();
+
