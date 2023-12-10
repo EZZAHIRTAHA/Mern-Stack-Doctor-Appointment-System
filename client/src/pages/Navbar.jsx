09b1b40539ccdedcb7a9298877b46cea89e6ca39
EZZAHIR TAHA <taha1.ezzahir@gmail.com>
2023-12-11 00:09:23 +0100

    Feat

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -3,50 +3,19 @@ import akdital from '/images/akdital.png'
 import toast from 'react-hot-toast'
 import { Close, Logout, MenuBar } from '../assets/icons'
 import axios from 'axios'
+import { logoutButton } from '../assets/classes'
 
 
 
 const Navbar = () => {
 
     const [navbarOpen, setNavbarOpen] = useState(false)
-    const [user, setUser] = useState(null)
 
     const handleToggle = () => {
       setNavbarOpen(!navbarOpen)
     }
 
-    const baseUrl = 'http://localhost:5000/api/user/get-user-info-by-id'
 
-    useEffect(() => {
-      const fetchUserData = async () => {
-        try {
-          const token = localStorage.getItem('token');
-          if (token) {
-            const response = await axios.get(baseUrl, {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-              },
-            });
-  
-            if (response.ok) {
-              const userData = await response.json();
-              setUser(userData.user);
-            } else {
-              // Handle error cases (e.g., token expired, server error)
-              console.error('Error fetching user data:', response.status);
-            }
-          }
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-        }
-      };
-  
-      fetchUserData();
-    }, []);
-    console.log(user)
-  
     const handleLogout = () => {
         localStorage.removeItem('token')
         toast.success('Logout successful')
@@ -80,13 +49,13 @@ const Navbar = () => {
               </ul>
             </div>
           )}
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+    <div className="hidden w-full md:block md:w-auto" >
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li className='text-white'>
           Welcome Taha
         </li>
         <li>
-          <button onClick={handleLogout} className="flex justify-center items-center gap-3 py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent  md:p-0 border-1 border-white " aria-current="page">
+          <button onClick={handleLogout} className={logoutButton} aria-current="page">
             <Logout className='text-2xl'/>
             Logout
           </button>
